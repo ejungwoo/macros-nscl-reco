57b3baaf57e67093be782615143976c282f87036
ejungwoo <phyjics@gmail.com>
2018-01-31 14:50:08 -0500

    make_summary.C and draw_plots.C for vaTrack and various cuts

--- draw_plots.C
+++ draw_plots.C
@@ -1,27 +1,27 @@
 #include "style.h"
 using namespace style;
 
-void draw_plots()
+void draw_plots
+(
+  TString branch = "trackChargeFix.1501.7e5c17a",
+  TString tag = "va"
+)
 {
-  gstat(0);
-  bool save_figures = false;
+  bool save_figures = true;
 
-  auto file = new TFile("data/summary.root");
-  //auto file = new TFile("data/summary.trackChargeFix.1499.d50474b.root");
+  TString branchtag = branch+"."+tag;
+  TString fileName = TString("data/summary.")+branchtag+".root";
+
+  //auto file = new TFile("data/summary.root");
+  auto file = new TFile(fileName,"read");
   auto tree = (TTree *) file -> Get("data");
 
-  TCut cut_good = "goodVertex && distVertex<10 && trackLength>500 && numdEdx>10";
-  TCut cut_good_pion = cut_good + "pdg==211";
-  TCut cut_good_proton = cut_good + "pdg==2212";
-  TCut cut_good_antiproton = cut_good_proton + "trackCharge<0";
-  TCut cut_good_antiproton2 = cut_good + "pdg!=211&&trackCharge<0";
-  TCut cut_good_positivepion = "trackCharge>0&&p<1000&&(dedx<-.1*(p-250)+20||dedx<-.5*(p-150)+40||dedx<20)&&p<600";
-
-  //TCut cut = cut_good + "abs(effCurvature2)>3";
-  TCut cut = cut_good;
-  //TCut cut = cut_good_antiproton2 + "p>-600";
-  //TCut cut = cut_good_pion + "trackCharge<0";
-  cout << "Cut is: " << cut << endl;
+  TCut cut_good = "goodVertex && distVertex<5 && trackLength>500 && numdEdx>20";
+  TCut cut_mpion = cut_good + "pdg==211&&trackCharge<0";
+  TCut cut_ppion = cut_good + "trackCharge>0&&p.Mag()<1000&&(dedx<-.1*(p.Mag()-250)+20||dedx<-.5*(p.Mag()-150)+40||dedx<20)&&p.Mag()<600";
+  TCut cut_proton = cut_good + "pdg==2212";
+  TCut cut_electron = cut_good + "p.Mag()<90&&dedx<30&&trackCharge<0";
+  TCut cut_positron = cut_good + "p.Mag()<90&&dedx<30&&trackCharge>0";
 
   /*
   //auto file_cut = new TFile("data/copy_positive-pion.root","recreate");
@@ -34,37 +34,14 @@ void draw_plots()
   gStyle -> SetPalette(kRainBow);
   TCanvas *cvs;
 
-  cvs = cc();
-  //auto h = make(new TH2D("pid",";p (MeV/c); dE/dx (ADC/mm);",500,-1500,3000,500,0,1000));
-  auto h = make(new TH2D("pid",";p (MeV/c); dE/dx (ADC/mm);",500,-800,2000,500,0,1000));
-  //h -> GetXaxis() -> SetNdivisions(505);
-  tree -> Draw("dedx:p>>pid",cut,"colz");
-  cvs -> SetLogz();
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
-  return;
-
-  cvs = c();
-  make(new TH1D("ec1","C1",200,-5000,10000));
-  tree -> Draw("-effCurvature1>>ec1",cut);
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
-
-  cvs = c();
-  make(new TH1D("ec2","C2",200,-50,100));
-  tree -> Draw("-effCurvature2>>ec2",cut);
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
-  
-  cvs = c();
-  make(new TH1D("ec3","C3",200,-100,200));
-  tree -> Draw("effCurvature3>>ec3",cut);
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
-
-  cvs = cc();
-  make(new TH2D("ec12","C1 vs C2",200,-5000,10000, 200,-50,100));
-  tree -> Draw("-effCurvature2:-effCurvature1>>ec12",cut,"colz");
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
-
-  cvs = cc();
-  make(new TH2D("ec13","C1 vs C3",200,-5000,10000, 200,-100,200));
-  tree -> Draw("effCurvature3:-effCurvature1>>ec13",cut,"colz");
-  if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
+  TCut cuts[] = {cut_good,cut_mpion,cut_ppion,cut_electron,cut_positron};
+  Int_t idx = 0;
+  for (TCut cut : cuts) {
+    auto name = Form("pid%d",idx++) + branchtag;
+    cvs = cc(name);
+    auto h = make(new TH2D(name,";p (MeV/c); dE/dx (ADC/mm);",500,-800,2000,500,0,1000));
+    tree -> Draw(TString("dedx:p.Mag()/trackCharge>>")+name,cut,"colz");
+    cvs -> SetLogz();
+    if (save_figures) cvs -> SaveAs(TString("figures/goodcut_")+cvs->GetName()+".png");
+  }
 }
--- make_summary.C
+++ make_summary.C
@@ -3,8 +3,8 @@ void make_summary()
   auto input_tree = new TChain("cbmsim");
   //TString tag = "trackChargeFix.1499.d50474b";  // before matching helix and reco track index
   //TString tag = "trackChargeFix.1500.8a7d00d";  // before format version fix
-  TString tag = "trackChargeFix.1501.7e5c17a";  // JungWoo - cut gg
-  //TString tag = "trackChargeFix.1502.9030344";  // PhysicsRun - use gg
+  //TString tag = "trackChargeFix.1501.7e5c17a";  // JungWoo - cut gg
+  TString tag = "trackChargeFix.1502.9030344";  // PhysicsRun - use gg
 
   Int_t runs[] = {2900,2901,2902,2903,2904,2905};
   Int_t numEventsInSplit = 2000;
@@ -21,40 +21,54 @@ void make_summary()
 
   input_tree -> SetBranchAddress("STVertex", &vertexArray);
   input_tree -> SetBranchAddress("STRecoTrack", &recoTrackArray);
-  input_tree -> SetBranchAddress("STVATrack", &vaTrackArray);
+  input_tree -> SetBranchAddress("VATracks", &vaTrackArray);
 
-  TFile *output_file = new TFile(TString("data/summary.")+tag+".root","recreate");
+  auto fileName = TString("data/summary.")+tag+".root";
+  TFile *output_file = new TFile(fileName,"recreate");
   TTree *output_tree = new TTree("data","");
 
   bool goodVertex;
-  Int_t runID, splitID, eventID, entry, trackID, numTracks, pdg, vertexID, trackCharge, numdEdx;
-  Double_t distVertex, distVertex2, dedx, trackLength, effCurvature1, effCurvature2, effCurvature3;
+  Int_t runID, splitID, eventID, entry, trackID, numTracks, vertexID;
+
+  //reco
+  Int_t trackCharge0, numdEdx0, pdg0;
+  Double_t distVertex0, distVertex20, dedx0, trackLength0, c10, c20;
+  TVector3 posVertex0, p0;
+
+  //va
+  Int_t trackCharge, numdEdx, pdg;
+  Double_t distVertex, distVertex2, dedx, trackLength, c1, c2;
   TVector3 posVertex, p;
 
   output_tree -> Branch("runID",&runID);
   output_tree -> Branch("splitID",&splitID);
   output_tree -> Branch("eventID",&eventID);
-
   output_tree -> Branch("entry",&entry);
+  output_tree -> Branch("trackID",&trackID);
   output_tree -> Branch("numTracks",&numTracks);
+  output_tree -> Branch("vertexID",&vertexID);
   output_tree -> Branch("goodVertex",&goodVertex);
   output_tree -> Branch("posVertex","TVector3",&posVertex);
 
-  output_tree -> Branch("trackID",&trackID);
-  output_tree -> Branch("p","TVector3", &p);
-  output_tree -> Branch("dedx",&dedx);
-  output_tree -> Branch("trackCharge",&trackCharge);
+  output_tree -> Branch("p0",          "TVector3",&p0);
+  output_tree -> Branch("dedx0",       &dedx0);
+  output_tree -> Branch("trackCharge0",&trackCharge0);
+  output_tree -> Branch("distVertex0", &distVertex0);
+  output_tree -> Branch("pdg0",        &pdg0);
+  output_tree -> Branch("numdEdx0",    &numdEdx0);
+  output_tree -> Branch("trackLength0",&trackLength0);
+  output_tree -> Branch("c10",         &c10);
+  output_tree -> Branch("c20",         &c20);
 
-  output_tree -> Branch("vertexID",&vertexID);
-  output_tree -> Branch("distVertex",&distVertex);
-  output_tree -> Branch("distVertex2",&distVertex2);
-
-  output_tree -> Branch("pdg",&pdg);
-  output_tree -> Branch("numdEdx",&numdEdx);
-  output_tree -> Branch("trackLength",&trackLength);
-  output_tree -> Branch("c1",&effCurvature1);
-  output_tree -> Branch("c2",&effCurvature2);
-  output_tree -> Branch("c3",&effCurvature3);
+  output_tree -> Branch("p",           "TVector3",&p);
+  output_tree -> Branch("dedx",        &dedx);
+  output_tree -> Branch("trackCharge", &trackCharge);
+  output_tree -> Branch("distVertex",  &distVertex);
+  output_tree -> Branch("pdg",         &pdg);
+  output_tree -> Branch("numdEdx",     &numdEdx);
+  output_tree -> Branch("trackLength", &trackLength);
+  output_tree -> Branch("c1",          &c1);
+  output_tree -> Branch("c2",          &c2);
 
   Int_t numEvents = input_tree -> GetEntries();
   for (entry = 0; entry < numEvents; ++entry)
@@ -84,36 +98,46 @@ void make_summary()
     if (posVertex.Z()<-9.49569&&posVertex.Z()>-12.80121&&posVertex.X()>-15&&posVertex.X()<15&&posVertex.Y()<-206.06&&posVertex.Y()>-246.06)
       goodVertex = true;
 
-    numTracks = recoTrackArray -> GetEntries();
+    numTracks = vaTrackArray -> GetEntries();
     for (trackID = 0; trackID < numTracks; ++trackID)
     {
-      auto track = (STRecoTrack *) recoTrackArray -> At(trackID);
-      auto pid = track -> GetPID();
-      pdg = STPID::GetPDG(pid);
+      auto track0 = (STRecoTrack *) vaTrackArray -> At(trackID);
+      auto track = (STRecoTrack *) vaTrackArray -> At(trackID);
+      pdg0 = STPID::GetPDG(track0 -> GetPID());
+      pdg = STPID::GetPDG(track -> GetPID());
 
       if (track -> GetVertexID() < 0) continue;
       vertexID = track -> GetVertexID();
 
+      auto posPOCAVertex0 = track0 -> GetPOCAVertex();
+      distVertex0 = sqrt(pow(posPOCAVertex0.X()-posVertex.X(),2)+pow(posPOCAVertex0.Y()-posVertex.Y(),2)+pow(posPOCAVertex0.Z()-posVertex.Z(),2));
+
       auto posPOCAVertex = track -> GetPOCAVertex();
       distVertex = sqrt(pow(posPOCAVertex.X()-posVertex.X(),2)+pow(posPOCAVertex.Y()-posVertex.Y(),2)+pow(posPOCAVertex.Z()-posVertex.Z(),2));
+      //auto posTargetPlane = track->GetPosTargetPlane();
+      //distVertex2 = sqrt(pow(posTargetPlane.X()-posVertex.X(),2)+pow(posTargetPlane.Y()-posVertex.Y(),2)+pow(posTargetPlane.Z()-posVertex.Z(),2));
 
-      auto posTargetPlane = track->GetPosTargetPlane();
-      distVertex2 = sqrt(pow(posTargetPlane.X()-posVertex.X(),2)+pow(posTargetPlane.Y()-posVertex.Y(),2)+pow(posTargetPlane.Z()-posVertex.Z(),2));
+      auto dedxArray0 = track0 -> GetdEdxPointArray();
+      numdEdx0 = dedxArray0 -> size();
 
-      auto dedxArray = track -> GetdEdxPointArray();
-      numdEdx = dedxArray -> size();
-      if (numdEdx < 4) continue;
-      auto dedxLast = dedxArray -> at(numdEdx-1);
-      trackLength = dedxLast.fLength;
+      if (numdEdx0 < 4) continue;
+      auto dedxFirst = dedxArray0 -> at(0);
+      auto dedxLast = dedxArray0 -> at(numdEdx-1);
+      trackLength0 = dedxLast.fLength - dedxFirst.fLength;
+      trackLength = dedxLast.fLength
 
-      effCurvature1 = track -> GetEffCurvature1();
-      effCurvature2 = track -> GetEffCurvature2();
-      effCurvature3 = track -> GetEffCurvature3();
+      c10 = track0 -> GetEffCurvature1();
+      c20 = track0 -> GetEffCurvature2();
+      c1  = track  -> GetEffCurvature1();
+      c2  = track  -> GetEffCurvature2();
 
-      trackCharge = track -> GetCharge();
+      trackCharge0 = track0 -> GetCharge();
+      trackCharge  = track  -> GetCharge();
 
-      p = track -> GetMomentum();
-      dedx = track -> GetdEdxWithCut(0,0.7);
+      p0 = track0 -> GetMomentum();
+      p  = track  -> GetMomentum();
+      dedx0 = track0 -> GetdEdxWithCut(0,0.7);
+      dedx  = track  -> GetdEdxWithCut(0,0.7);
 
       output_tree -> Fill();
     }
@@ -121,4 +145,5 @@ void make_summary()
 
   output_file -> cd();
   output_tree -> Write();
+  cout << ">> " << fileName << endl;
 }
